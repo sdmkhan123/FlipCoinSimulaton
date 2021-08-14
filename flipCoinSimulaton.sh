fd283dd2f31badcfaed40bf7e681b2d895d2f107
sdmkhan123 <sdmkhan126410@gmail.com>
2021-08-15 00:59:19 +0530

    Head count and Tail count

--- flipCoinSimulaton.sh
+++ flipCoinSimulaton.sh
@@ -1,9 +1,17 @@
 #! /bin/bash -x
 
-Flip=$((RANDOM%2))
-if [ $Flip -eq 1 ]
-then
-	echo "Head won"
-else
-	echo "Tail Won"
-fi
+read num
+HeadCount=0
+TailCount=0
+for (( i=0; i<n; i++ ))
+do
+	Flip=$((RANDOM%2))
+	if [ $Flip -eq 1 ]
+	then
+		((HeadCount++))
+	else
+		((TailCount++))
+	fi
+done
+echo "Number of Head won:" $HeadCount
+echo "Number of Tail won:" $TailCount
